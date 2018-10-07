254dfcc445a8e841e99007853c899885fade7110
Paul <paulmlee7@gmail.com>
2018-10-06 17:25:27 -0700

    Initial commit. Basic tic tac toe with nodeJS backend.

---
+++ public/main.js
@@ -0,0 +1,164 @@
+document.addEventListener('DOMContentLoaded', initializeGame);
+
+let gameGrid = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+let playerWon = false;
+let turnCounter = 0;
+let players = ["X","O"];
+//this will change based on who will go first.. later
+let playerTurn = 0;
+
+function initializeGame() {
+  addClickHandlers();
+}
+
+function addClickHandlers() {
+  let gridSquares = document.querySelectorAll(".gridSquare");
+  for (let gridSquaresIndex = 0; gridSquaresIndex < gridSquares.length; gridSquaresIndex++) {
+    gridSquares[gridSquaresIndex].addEventListener("click", () => {
+
+      console.log('row:', event.target.getAttribute("row"));
+      console.log('col:', event.target.getAttribute("col"));
+
+      let row = event.target.getAttribute("row");
+      let col = event.target.getAttribute("col");
+      
+      
+      gameGrid[row][col] = players[playerTurn];
+      event.target.textContent = players[playerTurn];
+      event.target.classList.add("disableClick");
+      // playerTurn = 1-playerTurn;
+
+      diagonalWinCheck("Player 1");
+      horizontalWinCheck(row, null, "Player 1");
+      verticalWinCheck(row, col, "Player 1");
+
+      turnCounter++
+      if (turnCounter === 9 && !playerWon) {
+        console.log('draw');
+
+        document.getElementById('gameTitle').textContent = "Draw"
+      } else if (!playerWon) {
+        computerAI('easy');
+      }
+
+      console.log(gameGrid);
+    });
+  }
+}
+
+//0,0
+function verticalWinCheck(row, col, player) {
+  let matchCounter = 0;
+
+  for (let i = 0; i <= 2; i++) {
+    if ((gameGrid[i][col] !== 0) && (gameGrid[0][col] === gameGrid[i][col])) {
+      matchCounter++
+    }
+  }
+
+  if (matchCounter === 3) {
+    disableAllSquares();
+    console.log("vertical winnnn");
+    playerWon = true;
+
+    document.getElementById('gameTitle').textContent = `${player} wins` 
+  }
+}
+
+function horizontalWinCheck(row, col, player) {
+  let matchCounter = 0;
+
+  for (let i = 1; i < gameGrid[row].length; i++) {
+    if ((gameGrid[row][i] !== 0) && (gameGrid[row][0] === gameGrid[row][i])) {
+      matchCounter++
+    }
+  }
+
+  if (matchCounter === 2) {
+    disableAllSquares();
+    console.log("horizontal winn");
+    playerWon = true;
+
+    document.getElementById('gameTitle').textContent = `${player} wins` 
+  }
+}
+
+function diagonalWinCheck(player) {
+  let matchCounter1 = 0;
+  let matchCounter2 = 0;
+
+  for (let i = 0; i <= 2; i++) {
+    if ((gameGrid[i][i] !== 0) && (gameGrid[0][0] === gameGrid[i][i])) {
+      matchCounter1++
+    }
+  }
+
+  for (let i = 0, k = 2; i <= 2, k >= 0; i++, k--) {
+    if ((gameGrid[i][k] !== 0) && (gameGrid[0][2] === gameGrid[i][k])) {
+      matchCounter2++
+    }
+  }
+
+  if (matchCounter1 === 3 || matchCounter2 === 3) {
+    disableAllSquares();
+    console.log("diagonal win");
+    playerWon = true;
+
+    document.getElementById('gameTitle').textContent = `${player} wins` 
+  }
+}
+
+function disableAllSquares() {
+  let gridSquares = document.querySelectorAll(".gridSquare");
+  for (let gridSquaresIndex = 0; gridSquaresIndex < gridSquares.length; gridSquaresIndex++) {
+    gridSquares[gridSquaresIndex].classList.add("disableClick");
+  }
+}
+
+function computerAI(mode) {
+  let gridSquares = document.querySelectorAll(".gridSquare");
+  let row = null;
+  let col = null;
+  let randomIndex = null;
+  let emptySquares = [];
+
+  console.log('ai start');
+  
+  for (let gridSquaresIndex = 0; gridSquaresIndex < gridSquares.length; gridSquaresIndex++) {
+    if (gridSquares[gridSquaresIndex].textContent === '') {
+      emptySquares.push(gridSquares[gridSquaresIndex]);
+      console.log('empty square found');
+    }
+  }
+  console.log(emptySquares);
+
+  if (mode === 'easy') {
+    randomIndex = Math.floor(Math.random() * emptySquares.length);
+    row = emptySquares[randomIndex].getAttribute("row");
+    col = emptySquares[randomIndex].getAttribute("col");
+  
+    gameGrid[row][col] = players[1];
+    emptySquares[randomIndex].textContent = players[1];
+    emptySquares[randomIndex].classList.add("disableClick");
+  } else {
+    //hard mode coding
+  }
+
+  turnCounter++
+  diagonalWinCheck("Player 2");
+  horizontalWinCheck(row, null, "Player 2");
+  verticalWinCheck(row, col, "Player 2");
+
+  if (turnCounter === 9 && !playerWon) {
+    console.log('draw');
+
+    document.getElementById('gameTitle').textContent = "Draw"
+  }
+  
+  console.log('ai stopped');
+}
---
+++ server/server.js
@@ -0,0 +1,67 @@
+console.log('--SERVER STARTED--');
+//part of node
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+//dependencies
+const _ = require('lodash');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+// middleware is like a 3rd party addon...
+// for express static middleware 
+const publicPath = path.join(__dirname, '../public');
+console.log(__dirname + '/../public');
+console.log(publicPath);
+
+// must call next() to complete middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  // fs.appendFile('server.log', log + '\n', (err) => {
+  //   if (err) {
+  //     console.log('Unable to append to server.log');
+  //   }
+  // });
+  next();
+});
+
+//serves up the html in public folder. static
+//public path is the path to the index.html
+app.use(express.static(publicPath));
+
+// Home route -- sending json
+// app.get('/', (req, res) => {
+//   res.send({
+//     name: 'Paul',
+//     stuff: [
+//       'games',
+//       'tv'
+//     ]
+//   });
+// });
+// About route.
+app.get('/about', (req, res) => {
+  res.send('about page');
+});
+// sending back JSON
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to handle request'
+  });
+});
+
+
+// listening on port 
+// nodemon serves app continuously
+app.listen(port, ()=> {
+  console.log(`server is up on ${port}`);
+});
+
+
+/*
+The order of the method calls determines what is rendered first..
+*/
